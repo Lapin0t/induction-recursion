f0583e490369904ae5311f6d85bee514bbdaf56d
Lapinot <peio.borthelle@ens-lyon.fr>
2018-06-04 15:38:12 +0100

    Add a non-whitespace replacement mode for the hiding script

--- doc/Hide.hs
+++ doc/Hide.hs
@@ -1,14 +1,17 @@
 module Main where
 
 seek :: String -> String
-seek ('{':'-':'<':'-':'}':s) = "     " ++ hide s
+seek ('{':'-':'<':'!':'-':'}':s) = "     " ++ hide True s
+seek ('{':'-':'<':'-':'}':s) = hide False s
 seek (c : s) = c : seek s
 seek [] = []
 
-hide :: String -> String
-hide ('{':'-':'>':'-':'}':s) = "     " ++ seek s
-hide (c : s) = ' ' : hide s
-hide [] = []
+hide :: Bool -> String -> String
+hide True ('{':'-':'>':'-':'}':s) = "     " ++ seek s
+hide False ('{':'-':'>':'-':'}':s) = seek s
+hide True (c : s) = ' ' : hide True s
+hide False (c : s) = hide False s
+hide _ [] = []
 
 main :: IO ()
 main = interact seek
